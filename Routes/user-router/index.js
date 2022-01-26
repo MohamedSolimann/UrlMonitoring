c3b6cc55b8e754af16df6aa84ae0731ba9bacd87
MohamedSolimann <mohamadico@gmail.com>
2022-01-26 19:20:28 +0200

    verified user token with jwt

--- Routes/user-router/index.js
+++ Routes/user-router/index.js
@@ -1,6 +1,9 @@
+const config = require("config");
+const jwt = require("jsonwebtoken");
 const userAuthentication = (req, res, next) => {
   let token = req.cookies["Token"];
-  if (!token) {
+  let authorizied = jwt.verify(token, config.get("secret"));
+  if (!authorizied) {
     res.status(401).json({ message: "User not authorizied" });
   } else {
     next();
