2dc9e29f1bf704c320b20991e0992174c216a80b
MohamedSolimann <mohamadico@gmail.com>
2022-01-26 13:49:19 +0200

    added create and signin user function and implemneted them for authentication purpose

--- Tests/reports/index.js
+++ Tests/reports/index.js
@@ -1,23 +1,31 @@
 const supertest = require("supertest");
 const app = require("../../index");
 const request = supertest(app);
+const { createNewUser, userSignIn } = require("../users/index");
 
 async function createNewReport() {
-  const response = await request.post("/reports").send({
-    url: "localhost:8080/",
-    status: 200,
-    availability: "100%",
-    outages: 0,
-    downtime: 0,
-    uptime: 1,
-    responsetime: 1,
-    history:
-      "Mon Jan 24 2022 19:18:18 GMT+0200 (Eastern European Standard Time)",
-  });
+  let newUser = await createNewUser();
+  let token = await userSignIn();
+  const response = await request
+    .post("/reports")
+    .send({
+      url: "localhost:8080/",
+      status: 200,
+      availability: "100%",
+      outages: 0,
+      downtime: 0,
+      uptime: 1,
+      responsetime: 1,
+      history:
+        "Mon Jan 24 2022 19:18:18 GMT+0200 (Eastern European Standard Time)",
+    })
+    .set("Cookie", token);
   return response.body.data;
 }
-async function deleteReport(reportId) {
-  const reponse = await request.delete(`/reports/${reportId}`);
+async function deleteReport(reportId, token) {
+  const reponse = await request
+    .delete(`/reports/${reportId}`)
+    .set("Cookie", token);
 }
 
 module.exports = { createNewReport, deleteReport };
