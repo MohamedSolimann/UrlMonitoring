346115d5ac8c2f457e88fb6e487d1dfb36423931
MohamedSolimann <mohamadico@gmail.com>
2022-01-26 13:35:58 +0200

    added create and signin user function and implemneted thenm for authentication purpose

--- Tests/checks/index.js
+++ Tests/checks/index.js
@@ -1,25 +1,31 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../../index");
+const { createNewUser, userSignIn } = require("../users/index");
 const request = supertest(app);
-
 async function createNewCheck() {
-  const response = await request.post("/checks").send({
-    _id: mongoose.Types.ObjectId(),
-    user_id: mongoose.Types.ObjectId(),
-    checkname: "test",
-    url: "testurl",
-    protocol: "testprotocol",
-    path: "testpath",
-    port: 1111,
-    webhook: "testwebhook",
-    tag: "testtag",
-    status: "Active",
-  });
+  let newUser = await createNewUser();
+  let token = await userSignIn();
+  const response = await request
+    .post("/checks")
+    .send({
+      user_id: mongoose.Types.ObjectId(),
+      checkname: "test",
+      url: "testurl",
+      protocol: "testprotocol",
+      path: "testpath",
+      port: 1111,
+      webhook: "testwebhook",
+      tag: "testtag",
+      status: "Active",
+    })
+    .set("Cookie", token);
   return response.body.data;
 }
-async function deleteCheck(checkId) {
-  const reponse = await request.delete(`/checks/${checkId}`);
+async function deleteCheck(checkId, token) {
+  const reponse = await request
+    .delete(`/checks/${checkId}`)
+    .set("Cookie", token);
 }
 
 module.exports = { createNewCheck, deleteCheck };
