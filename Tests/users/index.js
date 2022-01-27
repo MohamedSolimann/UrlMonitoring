e059b5ce7cee1e39a1ba8652f29327955820b97e
MohamedSolimann <mohamadico@gmail.com>
2022-01-27 19:14:44 +0200

    added verify user function

--- Tests/users/index.js
+++ Tests/users/index.js
@@ -2,12 +2,15 @@ const supertest = require("supertest");
 const app = require("../../index");
 const request = supertest(app);
 
+let userId, OTP;
 async function createNewUser() {
   const response = await request.post("/users").send({
     username: "mohamed",
     email: "a@a.com",
     password: "123123123",
   });
+  userId = response.body.data._id;
+  OTP = response.body.data.OTP;
   return response.body.data;
 }
 async function userSignIn() {
@@ -15,9 +18,16 @@ async function userSignIn() {
     email: "a@a.com",
     password: "123123123",
   });
+  console.log(response.body);
   return response.headers["set-cookie"][0];
+}
+async function verifyUser() {
+  const response = await request.post(`/users/auth/verify/${userId}`).send({
+    OTP,
+  });
+  return response.body.data;
 }
 async function deleteUser(userId, token) {
   const reponse = await request.delete(`/users/${userId}`).set("Cookie", token);
 }
-module.exports = { createNewUser, deleteUser, userSignIn };
+module.exports = { createNewUser, deleteUser, userSignIn, verifyUser };
