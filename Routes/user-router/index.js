1aa81816cf6943f3e996cc729ce37c5f4d964d93
MohamedSolimann <mohamadico@gmail.com>
2022-01-27 15:45:06 +0200

    updated verification logic in user authentication

--- Routes/user-router/index.js
+++ Routes/user-router/index.js
@@ -2,11 +2,15 @@ const config = require("config");
 const jwt = require("jsonwebtoken");
 const userAuthentication = (req, res, next) => {
   let token = req.cookies["Token"];
-  let authorizied = jwt.verify(token, config.get("secret"));
-  if (!authorizied) {
-    res.status(401).json({ message: "User not authorizied" });
+  if (token) {
+    let authorizied = jwt.verify(token, config.get("secret"));
+    if (!authorizied) {
+      res.status(401).json({ message: "User not authorizied" });
+    } else {
+      next();
+    }
   } else {
-    next();
+    res.status(401).json({ message: "User not authorizied" });
   }
 };
 module.exports = { userAuthentication };
