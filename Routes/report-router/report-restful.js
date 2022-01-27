01cadcd7100f9af4fb4cd9ac6ef74670fed6eaf7
MohamedSolimann <mohamadico@gmail.com>
2022-01-27 12:46:47 +0200

    removed unneccessary endpoints and functions

--- Routes/report-router/report-restful.js
+++ Routes/report-router/report-restful.js
@@ -2,53 +2,9 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const reportModel = require("../../Models/report-model/report-schema");
-const { updateRequestBody } = require("../../Models/report-model/index");
-const {
-  reportValidation,
-  catchValidationErrors,
-} = require("../../validation/report.validation");
+
 const { userAuthentication } = require("../user-router/index");
-router.post("/", reportValidation, catchValidationErrors, async (req, res) => {
-  const {
-    status,
-    availability,
-    uptime,
-    downtime,
-    responsetime,
-    history,
-    outages,
-    url,
-  } = req.body;
-  try {
-    let newReport = new reportModel({
-      _id: mongoose.Types.ObjectId(),
-      status,
-      availability,
-      uptime,
-      downtime,
-      responsetime,
-      history,
-      outages,
-      url,
-    });
-    await newReport.save();
-    res.status(201).json({ message: "Success", data: newReport });
-  } catch (error) {
-    res.status(500).json({ message: "Error", error });
-  }
-});
-router.get("/", userAuthentication, async (req, res) => {
-  try {
-    let reports = await reportModel.find({ deletedDate: null });
-    if (reports.length !== 0) {
-      res.status(200).json({ message: "Success", data: reports });
-    } else {
-      res.status(200).json({ message: "There is no reports!" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: "Error", error });
-  }
-});
+
 router.get("/:id", userAuthentication, async (req, res) => {
   const reportId = req.params.id;
   try {
@@ -68,38 +24,6 @@ router.get("/:id", userAuthentication, async (req, res) => {
     } else {
       res.status(500).json({ message: "Error", error });
     }
-  }
-});
-router.put("/:id", userAuthentication, async (req, res) => {
-  let reportId = req.params.id;
-  try {
-    let report = await reportModel.findOne({ _id: reportId });
-    if (report) {
-      let updatedBody = await updateRequestBody(req);
-      var updatedReport = await reportModel.findOneAndUpdate(
-        { _id: reportId },
-        { $set: updatedBody },
-        { new: true }
-      );
-    }
-    res.status(201).json({ message: "Success", data: updatedReport });
-  } catch (error) {
-    res.status(500).json({ message: "Error", error });
-  }
-});
-router.delete("/:id", userAuthentication, async (req, res) => {
-  let reportId = req.params.id;
-  try {
-    let report = await reportModel.findOne({ _id: reportId });
-    if (report) {
-      let deletedReport = await reportModel.findOneAndUpdate(
-        { _id: reportId },
-        { $set: { deletedDate: Date() } }
-      );
-    }
-    res.status(202).json({ message: "Success" });
-  } catch (error) {
-    res.status(500).json({ message: "Error" });
   }
 });
 
