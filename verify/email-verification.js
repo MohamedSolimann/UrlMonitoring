f48bffba38a358f39986479be801abc853ffb5c1
MohamedSolimann <mohamadico@gmail.com>
2022-01-27 16:22:50 +0200

    changed sendgrid token

--- verify/email-verification.js
+++ verify/email-verification.js
@@ -1,16 +1,9 @@
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(
-  "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"
+  "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"
 );
-const msg = {
-  to: "masoliman28@gmail.com", // Change to your recipient
-  from: "testersendgrid97@gmail.com", // Change to your verified sender
-  subject: "Sending with SendGrid is Fun",
-  text: "and easy to do anywhere, even with Node.js",
-  html: "<body><p> Example 1 - just the code (no localization in the message):</p>The verification code is: <strong>{{twilio_code}}</strong><p></p></body>",
-};
 
-function sendEmail() {
+function sendEmail(msg) {
   sgMail
     .send(msg)
     .then(() => {
